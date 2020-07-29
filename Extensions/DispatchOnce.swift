3c5925f2ced4e5f795ace59300f818ef5f4a3370
Choi <365473321@qq.com>
2020-07-29 11:34:19 +0800

    新增：代码在程序运行中只执行一次的功能

---
+++ Extensions/DispatchOnce.swift
@@ -0,0 +1,32 @@
+//
+//  DispatchOnce.swift
+//  ExtensionDemo
+//
+//  Created by Choi on 2020/7/29.
+//  Copyright © 2020 Choi. All rights reserved.
+//
+
+import Foundation
+
+struct Dispatch {
+    
+    private static var tokens = Set<String>()
+    
+    public static func once(file: String = #file,
+                            function: String = #function,
+                            line: Int = #line,
+                            execute: () -> Void) {
+        let token = "\(file):\(function):\(line)"
+        once(token: token, execute: execute)
+    }
+    
+    public static func once(token: String, execute: () -> Void) {
+        guard !tokens.contains(token) else { return }
+        objc_sync_enter(self)
+        defer {
+            objc_sync_exit(self)
+        }
+        tokens.insert(token)
+        execute()
+    }
+}
