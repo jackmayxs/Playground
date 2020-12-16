2599d8428ad46500ab1e61fb642e79c5bc57657f
Choi <365473321@qq.com>
2020-12-16 16:19:47 +0800

    新增Optional方法

--- Extensions/Foundation/FoundationPlus.swift
+++ Extensions/Foundation/FoundationPlus.swift
@@ -9,6 +9,37 @@
 import Foundation
 
 // MARK: - __________ Optional __________
+extension Optional {
+	
+	/// 解包Optional
+	/// - Throws: 解包失败抛出错误
+	/// - Returns: Wrapped Value
+	func unwrap() throws -> Wrapped {
+		guard let unwrapped = self else {
+			throw OptionalError.badValue
+		}
+		return unwrapped
+	}
+	
+	/// 解包Optional
+	/// - Parameter defaultValue: 自动闭包
+	/// - Returns: Wrapped Value
+	func unwrap(ifNone defaultValue: @autoclosure () -> Wrapped) -> Wrapped {
+		guard let unwrapped = try? unwrap() else {
+			return defaultValue()
+		}
+		return unwrapped
+	}
+	
+	/// Optional Error
+	enum OptionalError: LocalizedError {
+		case badValue
+		
+		var errorDescription: String? {
+			"Bad \(Wrapped.self)."
+		}
+	}
+}
 extension Optional where Wrapped == String {
 	var safeValue: String { self ?? "" }
 	
