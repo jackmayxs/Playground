150fbd12d2846e8b719cef825c5874d78dc4dfb2
Choi <365473321@qq.com>
2021-01-07 18:29:11 +0800

    调整代码结构 | 新增字符串计算属性

---
+++ Extensions/Foundation/DatePlus.swift
@@ -0,0 +1,35 @@
+//
+//  DatePlus.swift
+//  ExtensionDemo
+//
+//  Created by Choi on 2021/1/7.
+//  Copyright © 2021 Choi. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+	
+	/// 计算两个日期之间相差的DateComponents
+	/// - 注意：两个日期的先后顺序，如果开始日期晚于结束日期，返回的DateComponents里的元素将为负数
+	/// - Parameters:
+	///   - lhs: 开始时间
+	///   - rhs: 结束时间
+	/// - Returns: DateComponents
+	static func >> (lhs: Date, rhs: Date) -> DateComponents {
+		Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: lhs, to: rhs)
+	}
+	
+	/// 返回当前时间
+	static var now: Date { Date() }
+	
+	/// 返回当前时间只包含小时的时间
+	static var hourOfClock: Date {
+		let cal = Calendar.current
+		var components = cal.dateComponents(in: .current, from: .now)
+		components.minute = 0
+		components.second = 0
+		components.nanosecond = 0
+		return cal.date(from: components).unsafelyUnwrapped
+	}
+}
---
+++ Extensions/Foundation/StringPlus.swift
@@ -0,0 +1,53 @@
+//
+//  StringPlus.swift
+//  ExtensionDemo
+//
+//  Created by Choi on 2021/1/7.
+//  Copyright © 2021 Choi. All rights reserved.
+//
+
+import Foundation
+
+extension Optional where Wrapped == String {
+	
+	var safeValue: String { self ?? "" }
+	
+	/// 判断Optional<String>类型是否为空
+	var isEmptyString: Bool {
+		switch self {
+			case .some(let string): return string.isEmptyString
+			case .none: return true
+		}
+	}
+	
+	/// 返回不为空字符串的Optional<String>
+	var validString: Self {
+		isEmptyString ? .none : unsafelyUnwrapped
+	}
+}
+
+infix operator <-- : MultiplicationPrecedence
+infix operator --> : MultiplicationPrecedence
+extension String {
+	
+	/// 使用右侧的字符串
+	/// - Parameters:
+	///   - lhs: 左操作对象
+	///   - rhs: 右操作对象
+	/// - Note: 以下两个方法对于字典类型在合并其他字典时的回调闭包里使用语法糖时较为有用
+	/// - Example:　aDict.merging(anotherOne) { $0 <-- $1 } // 使用当前值(如果直接返回$0会触发编译器错误)
+	static func --> (lhs: String, rhs: String) -> String { rhs }
+	
+	/// 使用左侧的字符串
+	/// - Parameters:
+	///   - lhs: 左操作对象
+	///   - rhs: 右操作对象
+	static func <-- (lhs: String, rhs: String) -> String { lhs }
+}
+// MARK: - __________ Verification __________
+extension String {
+	
+	var isEmptyString: Bool {
+		trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+	}
+}
