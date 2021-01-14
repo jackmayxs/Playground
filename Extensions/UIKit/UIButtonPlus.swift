cf5b42bdd7fcdf6379d88a38f1db576e8cd28d90
Choi <365473321@qq.com>
2021-01-14 23:12:12 +0800

    新增UIButton扩展属性 | 优化命名

--- Extensions/UIKit/UIButtonPlus.swift
+++ Extensions/UIKit/UIButtonPlus.swift
@@ -16,19 +16,32 @@ extension UIButton {
 		case ㊨㊧
 	}
 	
-	private struct AssociatedObjectKeys {
-		static var ImageTitleSpacingKey = "ImageTitleSpacingKey"
-		static var ImageTitleStyleKey = "ImageTitleStyleKey"
+	private enum Property {
+		static var imageTitleStyle = UUID()
+		static var imageTitleSpacing = UUID()
+		static var useBackgroundImageSize = UUID()
 	}
 	
 	// MARK: - Properties
 	
-	private(set) var imageTitleStyle: ImageTitleStyle {
-		set(style) {
-			objc_setAssociatedObject(self, &AssociatedObjectKeys.ImageTitleStyleKey, style.rawValue, .OBJC_ASSOCIATION_ASSIGN)
+	var useBackgroundImageSize: Bool {
+		set {
+			objc_setAssociatedObject(self, &Property.useBackgroundImageSize, newValue, .OBJC_ASSOCIATION_ASSIGN)
 		}
 		get {
-			guard let rawValue = objc_getAssociatedObject(self, &AssociatedObjectKeys.ImageTitleStyleKey) as? Int else {
+			guard let boolValue = objc_getAssociatedObject(self, &Property.useBackgroundImageSize) as? Bool else {
+				return false
+			}
+			return boolValue
+		}
+	}
+	
+	private(set) var imageTitleStyle: ImageTitleStyle {
+		set(style) {
+			objc_setAssociatedObject(self, &Property.imageTitleStyle, style.rawValue, .OBJC_ASSOCIATION_ASSIGN)
+		}
+		get {
+			guard let rawValue = objc_getAssociatedObject(self, &Property.imageTitleStyle) as? Int else {
 				return .㊧㊨
 			}
 			return ImageTitleStyle(rawValue: rawValue)!
@@ -37,37 +50,39 @@ extension UIButton {
 	
 	private(set) var imageTitleSpacing: CGFloat {
 		set {
-			objc_setAssociatedObject(self, &AssociatedObjectKeys.ImageTitleSpacingKey, newValue, .OBJC_ASSOCIATION_ASSIGN)
+			objc_setAssociatedObject(self, &Property.imageTitleSpacing, newValue, .OBJC_ASSOCIATION_ASSIGN)
 		}
 		get {
-			objc_getAssociatedObject(self, &AssociatedObjectKeys.ImageTitleSpacingKey) as? CGFloat ?? 0
+			objc_getAssociatedObject(self, &Property.imageTitleSpacing) as? CGFloat ?? 0
 		}
 	}
 	
 	var imageWidth: CGFloat {
 		guard let image = imageView?.image else {
-			// 确保title 为空 | 否则返回 0
-			guard titleLabel?.text?.isEmpty ?? true else { return 0 }
-			// 返回背景图的宽度
-			return currentBackgroundImage?.size.width ?? 0
+			if useBackgroundImageSize {
+				return currentBackgroundImage?.size.width ?? 0
+			} else {
+				return 0
+			}
 		}
 		return image.size.width
 	}
 	
 	var imageHeight: CGFloat {
 		guard let image = imageView?.image else {
-			// 确保title 为空 | 否则返回 0
-			guard titleLabel?.text?.isEmpty ?? true else { return 0 }
-			// 返回背景图的高度
-			return currentBackgroundImage?.size.height ?? 0
+			if useBackgroundImageSize {
+				return currentBackgroundImage?.size.height ?? 0
+			} else {
+				return 0
+			}
 		}
 		return image.size.height
 	}
 	
 	var titleWidth: CGFloat {
-        // 以下两行: 适配iOS14,否则此属性会按照字体的Font返回一个值,从而影响intrinsicContentSize的计算
-        guard let titleLabel = titleLabel else { return 0 }
-        guard titleLabel.text != .none else { return 0 }
+		// 以下两行: 适配iOS14,否则此属性会按照字体的Font返回一个值,从而影响intrinsicContentSize的计算
+		guard let titleLabel = titleLabel else { return 0 }
+		guard titleLabel.text != .none else { return 0 }
 		if #available(iOS 8.0, *) {
 			return titleLabel.intrinsicContentSize.width
 		} else {
@@ -76,9 +91,9 @@ extension UIButton {
 	}
 	
 	var titleHeight: CGFloat {
-        // 以下两行: 适配iOS14,否则此属性会按照字体的Font返回一个值,从而影响intrinsicContentSize的计算
-        guard let titleLabel = titleLabel else { return 0 }
-        guard titleLabel.text != .none else { return 0 }
+		// 以下两行: 适配iOS14,否则此属性会按照字体的Font返回一个值,从而影响intrinsicContentSize的计算
+		guard let titleLabel = titleLabel else { return 0 }
+		guard titleLabel.text != .none else { return 0 }
 		if #available(iOS 8.0, *) {
 			return titleLabel.intrinsicContentSize.height
 		} else {
@@ -94,12 +109,12 @@ extension UIButton {
 		
 		// 计算按钮宽高
 		switch imageTitleStyle  {
-			case .㊤㊦, .㊦㊤:
-				intrinsicWidth = max(imageWidth, titleWidth)
-				intrinsicHeight = imageHeight + imageTitleSpacing + titleHeight
-			case .㊧㊨, .㊨㊧:
-				intrinsicWidth = imageWidth + imageTitleSpacing + titleWidth
-				intrinsicHeight = max(imageHeight, titleHeight)
+		case .㊤㊦, .㊦㊤:
+			intrinsicWidth = max(imageWidth, titleWidth)
+			intrinsicHeight = imageHeight + imageTitleSpacing + titleHeight
+		case .㊧㊨, .㊨㊧:
+			intrinsicWidth = imageWidth + imageTitleSpacing + titleWidth
+			intrinsicHeight = max(imageHeight, titleHeight)
 		}
 		
 		return CGSize(width: intrinsicWidth, height: intrinsicHeight) + contentEdgeInsets
@@ -128,94 +143,94 @@ extension UIButton {
 		/// - 说明:
 		///  - 平时使用最多的就是水平和垂直方向都居中的情况了,其他情况只为了加深印象
 		switch (contentVerticalAlignment, contentHorizontalAlignment) {
-			case (.center, .center):
-				switch style {
-					case .㊤㊦:
-						imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeight-spacing, right: 0)
-					case .㊧㊨:
-						let offset = spacing/2
-						imageInsets = UIEdgeInsets(top: 0, left: -offset, bottom: 0, right: offset)
-						titleInsets = UIEdgeInsets(top: 0, left: offset, bottom: 0, right: -offset)
-					case .㊦㊤:
-						imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: 0, right: 0)
-					case .㊨㊧:
-						let imageOffset = titleWidth + spacing/2
-						let titleOffset = imageWidth + spacing/2
-						imageInsets = UIEdgeInsets(top: 0, left: imageOffset, bottom: 0, right: -imageOffset)
-						titleInsets = UIEdgeInsets(top: 0, left: -titleOffset, bottom: 0, right: titleOffset)
-				}
-			case (.top, .left):
-				switch style {
-					case .㊤㊦:
-						titleInsets = UIEdgeInsets(top: imageHeight+spacing, left: -imageWidth, bottom: 0, right: 0)
-					case .㊧㊨:
-						titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
-					case .㊦㊤:
-						imageInsets = UIEdgeInsets(top: titleHeight+spacing, left: 0, bottom: -titleHeight-spacing, right: 0)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
-					case .㊨㊧:
-						imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
-				}
-			case (.top, .right):
-				switch style {
-					case .㊤㊦:
-						imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: imageHeight+spacing, left: -imageWidth-spacing, bottom: 0, right: 0)
-					case .㊧㊨:
-						imageInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
-					case .㊦㊤:
-						imageInsets = UIEdgeInsets(top: titleHeight+spacing, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
-					case .㊨㊧:
-						imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth-spacing, bottom: 0, right: imageWidth+spacing)
-				}
-			case (.bottom, .left):
-				switch style {
-					case .㊤㊦:
-						imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: titleHeight+spacing, right: 0)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
-					case .㊧㊨:
-						titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
-					case .㊦㊤:
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: imageHeight+spacing, right: 0)
-					case .㊨㊧:
-						imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
-				}
-			case (.bottom, .right):
-				switch style {
-					case .㊤㊦:
-						imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: titleHeight+spacing, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
-					case .㊧㊨:
-						imageInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
-					case .㊦㊤:
-						imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: imageHeight+spacing, right: 0)
-					case .㊨㊧:
-						imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth-spacing, bottom: 0, right: imageWidth+spacing)
-				}
-			case (.center, .left):
-				switch style {
-					case .㊤㊦:
-						imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: 0, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeight-spacing, right: 0)
-					case .㊧㊨:
-						titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
-					case .㊦㊤:
-						imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
-						titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: 0, right: 0)
-					case .㊨㊧:
-						imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
-						titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
-				}
-			default:
-				break
+		case (.center, .center):
+			switch style {
+			case .㊤㊦:
+				imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeight-spacing, right: 0)
+			case .㊧㊨:
+				let offset = spacing/2
+				imageInsets = UIEdgeInsets(top: 0, left: -offset, bottom: 0, right: offset)
+				titleInsets = UIEdgeInsets(top: 0, left: offset, bottom: 0, right: -offset)
+			case .㊦㊤:
+				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: 0, right: 0)
+			case .㊨㊧:
+				let imageOffset = titleWidth + spacing/2
+				let titleOffset = imageWidth + spacing/2
+				imageInsets = UIEdgeInsets(top: 0, left: imageOffset, bottom: 0, right: -imageOffset)
+				titleInsets = UIEdgeInsets(top: 0, left: -titleOffset, bottom: 0, right: titleOffset)
+			}
+		case (.top, .left):
+			switch style {
+			case .㊤㊦:
+				titleInsets = UIEdgeInsets(top: imageHeight+spacing, left: -imageWidth, bottom: 0, right: 0)
+			case .㊧㊨:
+				titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
+			case .㊦㊤:
+				imageInsets = UIEdgeInsets(top: titleHeight+spacing, left: 0, bottom: -titleHeight-spacing, right: 0)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
+			case .㊨㊧:
+				imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
+			}
+		case (.top, .right):
+			switch style {
+			case .㊤㊦:
+				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: imageHeight+spacing, left: -imageWidth-spacing, bottom: 0, right: 0)
+			case .㊧㊨:
+				imageInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
+			case .㊦㊤:
+				imageInsets = UIEdgeInsets(top: titleHeight+spacing, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
+			case .㊨㊧:
+				imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth-spacing, bottom: 0, right: imageWidth+spacing)
+			}
+		case (.bottom, .left):
+			switch style {
+			case .㊤㊦:
+				imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: titleHeight+spacing, right: 0)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
+			case .㊧㊨:
+				titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
+			case .㊦㊤:
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: imageHeight+spacing, right: 0)
+			case .㊨㊧:
+				imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
+			}
+		case (.bottom, .right):
+			switch style {
+			case .㊤㊦:
+				imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: titleHeight+spacing, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: 0)
+			case .㊧㊨:
+				imageInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
+			case .㊦㊤:
+				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: imageHeight+spacing, right: 0)
+			case .㊨㊧:
+				imageInsets = UIEdgeInsets(top: 0, left: titleWidth, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth-spacing, bottom: 0, right: imageWidth+spacing)
+			}
+		case (.center, .left):
+			switch style {
+			case .㊤㊦:
+				imageInsets = UIEdgeInsets(top: -titleHeight-spacing, left: 0, bottom: 0, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: -imageHeight-spacing, right: 0)
+			case .㊧㊨:
+				titleInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
+			case .㊦㊤:
+				imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: -titleHeight-spacing, right: -titleWidth)
+				titleInsets = UIEdgeInsets(top: -imageHeight-spacing, left: -imageWidth, bottom: 0, right: 0)
+			case .㊨㊧:
+				imageInsets = UIEdgeInsets(top: 0, left: titleWidth+spacing, bottom: 0, right: -titleWidth-spacing)
+				titleInsets = UIEdgeInsets(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
+			}
+		default:
+			break
 		}
 		
 		// 赋值
