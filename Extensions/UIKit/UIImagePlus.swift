363790a27072a66369dd1f7ddea4bd35ba1227cb
Choi <365473321@qq.com>
2021-04-30 09:48:29 +0800

    新增UIImage扩展方法

--- Extensions/UIKit/UIImagePlus.swift
+++ Extensions/UIKit/UIImagePlus.swift
@@ -11,6 +11,29 @@ import Photos
 
 extension UIImage {
 	
+	//将图片缩放成指定尺寸（多余部分自动删除）
+	func scaled(to newSize: CGSize) -> UIImage? {
+		//计算比例
+		let aspectWidth  = newSize.width/size.width
+		let aspectHeight = newSize.height/size.height
+		let aspectRatio = max(aspectWidth, aspectHeight)
+		
+		//图片绘制区域
+		var canvas = CGRect.zero
+		canvas.size.width  = size.width * aspectRatio
+		canvas.size.height = size.height * aspectRatio
+		canvas.origin.x = (newSize.width - size.width * aspectRatio) / 2.0
+		canvas.origin.y = (newSize.height - size.height * aspectRatio) / 2.0
+		
+		//绘制并获取最终图片
+		UIGraphicsBeginImageContext(newSize)
+		defer {
+			UIGraphicsEndImageContext()
+		}
+		draw(in: canvas)
+		return UIGraphicsGetImageFromCurrentImageContext()
+	}
+	
 	func saveToPhotoLibrary(completionHandler: @escaping (Bool, Error?) -> Void) {
 		PHPhotoLibrary.shared().performChanges {
 			PHAssetChangeRequest.creationRequestForAsset(from: self)
