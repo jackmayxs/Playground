c636d4152c522ac7e22fbc6bb46d895bef05c299
Choi <365473321@qq.com>
2021-05-13 11:13:47 +0800

    修改：UIStackView方法名

--- Extensions/UIKit/UIStackViewPlus.swift
+++ Extensions/UIKit/UIStackViewPlus.swift
@@ -12,22 +12,22 @@ extension UIStackView {
 	
 	/// Remove all of the arranged subviews
 	func purgeArrangedSubviews() {
-		arrangedSubviews.forEach { sub in
-			removeArrangedSubview(sub)
-			sub.removeFromSuperview()
+		arrangedSubviews.forEach { subview in
+			removeArrangedSubview(subview)
+			subview.removeFromSuperview()
 		}
 	}
 	
-    func addBackgroundView(_ backgroundView: UIView) {
-        backgroundView.frame = bounds
-        backgroundView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        insertSubview(backgroundView, at: 0)
+    func addBackground(view: UIView) {
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        insertSubview(view, at: 0)
     }
     /// 添加StackView背景视图颜色
     /// - Parameter color: 背景View颜色
-    func addBackground(_ color: UIColor) {
-        let backgroundView = UIView(frame: bounds)
-        backgroundView.backgroundColor = color
-        addBackgroundView(backgroundView)
+    func addBackground(color: UIColor) {
+        let colored = UIView(frame: bounds)
+        colored.backgroundColor = color
+		addBackground(view: colored)
     }
 }
