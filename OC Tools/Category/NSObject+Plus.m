5f582db792adf6de36ba648aa64c29cd10b2e00f
Choi <365473321@qq.com>
2021-08-09 11:51:55 +0800

    OC Tools：优化方法交换

--- OC Tools/Category/NSObject+Plus.m
+++ OC Tools/Category/NSObject+Plus.m
@@ -10,12 +10,38 @@
 @implementation NSObject (Plus)
 
 + (void)exchangeSelector:(SEL)original withSwizzled:(SEL)swizzled {
+	
     Method originalMethod = class_getInstanceMethod(self, original);
     Method swizzledMethod = class_getInstanceMethod(self, swizzled);
-    if (class_addMethod(self, original, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))) {
-        class_replaceMethod(self, swizzled, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+	
+	IMP originalIMP = method_getImplementation(originalMethod);
+	IMP swizzledIMP = method_getImplementation(swizzledMethod);
+	
+	const char *originalEncoding = method_getTypeEncoding(originalMethod);
+	const char *swizzledEncoding = method_getTypeEncoding(swizzledMethod);
+	
+	// 如果原方法没有实现
+	if (!originalMethod) {
+		// 先用新的实现来，临时添加一个（这里忽略新实现也为空的情况，可以类似的处理）
+		class_addMethod(self, original, swizzledIMP, swizzledEncoding);
+		// 对 originalMethod 变量重新赋值
+		originalMethod = class_getInstanceMethod(self, original);
+		// 创建默认实现，可以进行一些日志收集之类的
+		IMP defaultIMP = imp_implementationWithBlock(^(id self, SEL _cmd) {
+			NSLog(@"一些默认实现...");
+		});
+		// 为originalMethod设置IMP
+		method_setImplementation(originalMethod, defaultIMP);
+	}
+	
+	// 添加成功即：原本没有 originalSelector，成功为子类添加了一个 originalSelector - swizzledMethod 的方法
+    if (class_addMethod(self, original, swizzledIMP, swizzledEncoding)) {
+		// 这里将原 swizzledIMP 替换为 originalIMP
+        class_replaceMethod(self, swizzled, originalIMP, originalEncoding);
+		NSLog(@"添加成功!");
     }
-    else {
+	// 已经存在原方法,直接交换方法实现
+	else {
         method_exchangeImplementations(originalMethod, swizzledMethod);
     }
 }
