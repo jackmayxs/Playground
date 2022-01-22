5c18bf46a2cc98b044151dfe83bbc4aef801f220
Choi <365473321@qq.com>
2022-01-22 14:26:25 +0800

    新增：FrameBootcamp

---
+++ ExtensionDemo/BestPractice/SwiftUI/FrameBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  FrameBootcamp.swift
+//  ExtensionDemo
+//
+//  Created by Choi on 2022/1/22.
+//  Copyright © 2022 Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+//			.background(.green)
+//			.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+
+			.background(.red)
+			.frame(height: 100, alignment: .top)
+			.background(.orange)
+			.frame(width: 150)
+			.background(.purple)
+			.frame(maxWidth: .infinity, alignment: .leading)
+			.background(.pink)
+			.frame(height: 400)
+			.background(.green)
+			.frame(maxHeight: .infinity, alignment: .top)
+			.background(.yellow)
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}
