a0b91d0a50c2367077a123abb887b34da4d64960
Choi <365473321@qq.com>
2022-08-18 12:52:09 +0800

    扩展控制器方法

--- Extensions/UIKit/UIViewControllerPlus.swift
+++ Extensions/UIKit/UIViewControllerPlus.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+extension UINavigationController {
+    
+    /// 新控制器压栈 | 移除所有以前的控制器
+    /// - Parameters:
+    ///   - newRootController: 新的根控制器
+    func refillNavigationStack(_ newRootController: UIViewController, animated: Bool = true) {
+        let previousControllersCount = viewControllers.count
+        pushViewController(newRootController, animated: animated)
+        viewControllers.removeFirst(previousControllersCount)
+    }
+    
+    /// 替换顶层控制器
+    /// - Parameters:
+    ///   - newController: 顶层控制器
+    func replaceTopViewController(_ newController: UIViewController, animated: Bool = true) {
+        let lastTopControllerIndex = viewControllers.count - 1
+        pushViewController(newController, animated: animated)
+        viewControllers.remove(at: lastTopControllerIndex)
+    }
+}
+
 extension UIViewController {
     
     func push(_ controller: UIViewController, animated: Bool = true) {
