1f2dceaba6275545ef5262f780ea43a1561492fc
Choi <365473321@qq.com>
2022-08-21 17:39:48 +0800

    优化PropertyWrapper

--- Extensions/Foundation/PropertyWrappers.swift
+++ Extensions/Foundation/PropertyWrappers.swift
@@ -111,27 +111,23 @@ final class Temporary<T> {
 	typealias ValueBuilder = () -> T
 	
 	private var value: T?
-	private lazy var timer = GCDTimer.scheduledTimer(
-		delay: .now() + survivalTime,
-		queue: .global(qos: .background)
-	) { _ in
-		self.value = .none
-	}
-	private let survivalTime: TimeInterval
-	private let builder: ValueBuilder
-	init(wrappedValue: @escaping ValueBuilder, expireIn survivalTime: TimeInterval) {
-		self.builder = wrappedValue
-		self.survivalTime = survivalTime
-	}
-	var wrappedValue: T {
-		defer {
-			// 每次调用都推迟执行
-			timer.fire(.now() + survivalTime)
-		}
-		guard let unwrapped = value else {
-			value = builder()
-			return value.unsafelyUnwrapped
-		}
-		return unwrapped
-	}
+    private let survivalTime: TimeInterval
+    private let builder: ValueBuilder
+    private lazy var timer = GCDTimer.scheduledTimer(delay: .now() + survivalTime, queue: .global(qos: .utility)) { _ in
+        self.value = .none
+    }
+    init(wrappedValue: @escaping ValueBuilder, expireIn survivalTime: TimeInterval) {
+        self.builder = wrappedValue
+        self.survivalTime = survivalTime
+    }
+    var wrappedValue: T {
+        defer { /// 每次调用都推迟执行
+            timer.fire(.now() + survivalTime)
+        }
+        guard let unwrapped = value else {
+            value = builder()
+            return value.unsafelyUnwrapped
+        }
+        return unwrapped
+    }
 }
