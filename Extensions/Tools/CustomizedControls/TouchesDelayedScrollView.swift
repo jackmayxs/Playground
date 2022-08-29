53e722e9e628602ac082df617a7056860c5885eb
Choi <365473321@qq.com>
2022-08-29 20:34:16 +0800

    优化ScrollView基类

--- Extensions/Tools/CustomizedControls/TouchesDelayedScrollView.swift
+++ Extensions/Tools/CustomizedControls/TouchesDelayedScrollView.swift
@@ -10,6 +10,18 @@ import UIKit
 
 final class TouchesDelayedScrollView: UIScrollView {
 	
+    /// 避免如像UISlider类似的控件在滑动时被UIScrollView滑动事件阻断的问题
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let receiver = super.hitTest(point, with: event)
+        let isUIControl = receiver?.isKind(of: UIControl.self) ?? false
+        if isUIControl {
+            isScrollEnabled = false
+        } else {
+            isScrollEnabled = true
+        }
+        return receiver
+    }
+    
 	/// 重写此方法, 可延迟子视图中点击事件, 避免滑动和点击事件冲突
 	override func touchesShouldCancel(in view: UIView) -> Bool {
 		if view.isKind(of: UIControl.self) {
