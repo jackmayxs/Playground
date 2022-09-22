aecd696f086a1854ca133248460f58a1232e61f3
Choi <365473321@qq.com>
2022-09-22 11:19:29 +0800

    新增：BaseStandardTableViewController基类

---
+++ Extensions/Base/Controller/BaseStandardTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseStandardTableViewController.swift
+//  zeniko
+//
+//  Created by Choi on 2022/9/22.
+//
+//  带一个默认Cell类型的表格视图
+
+import UIKit
+
+class BaseStandardTableViewController<Cell: UITableViewCell>: BaseTableViewController {
+
+    override func configureTableView(_ tableView: UITableView) {
+        super.configureTableView(tableView)
+        Cell.registerFor(tableView)
+    }
+    
+    func configureCell(_ cell: Cell) {}
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Cell.dequeueReusableCell(from: tableView, indexPath: indexPath)
+        configureCell(cell)
+        return cell
+    }
+}
