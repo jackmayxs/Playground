3c819e38659aadf7ee71e3485350cef23728f7a3
Choi <365473321@qq.com>
2022-10-21 14:08:39 +0800

    调整文件结构

---
+++ Extensions/Foundation/ArrayPlus.swift
@@ -0,0 +1,106 @@
+//
+//  ArrayPlus.swift
+//  zeniko
+//
+//  Created by Choi on 2022/10/21.
+//
+
+import Foundation
+
+extension Array {
+    
+    init(@ArrayBuilder<Element> _ itemsBuilder: () -> Self) {
+        self.init(itemsBuilder())
+    }
+    
+    public mutating func append(@ArrayBuilder<Element> _ builder: () -> [Element]) {
+        let elements = builder()
+        append(contentsOf: elements)
+    }
+    
+    public func itemAt(_ index: Index) -> Element? {
+        guard index < count else { return nil }
+        return self[index]
+    }
+    
+    public init(generating elementGenerator: (Int) -> Element, count: Int) {
+        self = (0..<count).map(elementGenerator)
+    }
+    public init(generating elementGenerator: () -> Element, count: Int) {
+        self = (0..<count).map { _ in
+            elementGenerator()
+        }
+    }
+}
+
+// MARK: - Equatable Array
+extension Array where Element: Equatable {
+    
+    @discardableResult
+    /// 替换指定元素
+    /// - Parameter newElement: 新元素
+    /// - Returns: 是否替换成功
+    mutating func replaceElement(_ newElement: Element) -> Bool {
+        if let index = firstIndex(of: newElement) {
+            let range = index..<index + 1
+            let updated = [newElement]
+            replaceSubrange(range, with: updated)
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// MARK: - Hashable Array
+extension Array where Element : Hashable {
+    
+    mutating func appendUnique<S>(contentsOf newElements: S) where Element == S.Element, S : Sequence {
+        newElements.forEach { element in
+            appendUnique(element)
+        }
+    }
+    
+    /// 添加唯一的元素
+    /// - Parameter newElement: 遵循Hashable的元素
+    mutating func appendUnique(_ newElement: Element) {
+        let isNotUnique = contains { element in
+            element.hashValue == newElement.hashValue
+        }
+        guard !isNotUnique else { return }
+        append(newElement)
+    }
+    
+    /// 合并唯一的元素 | 可用于reduce
+    /// - Parameters:
+    ///   - lhs: 数组
+    ///   - rhs: 要添加的元素
+    /// - Returns: 结果数组
+    static func +> (lhs: Self, rhs: Element) -> Self {
+        var result = lhs
+        result.appendUnique(rhs)
+        return result
+    }
+    
+    /// 合并数组
+    /// - Parameters:
+    ///   - lhs: 原数组
+    ///   - rhs: 新数组
+    /// - Returns: 合并唯一元素的数组
+    static func +> (lhs: Self, rhs: Self) -> Self {
+        rhs.reduce(lhs, +>)
+    }
+    
+    var set: Set<Element> {
+        Set(self)
+    }
+}
+
+// MARK: - ArraySlice
+extension ArraySlice {
+    
+    /// 转换为Array
+    var array: Array<Element> {
+        Array(self)
+    }
+}
---
+++ Extensions/Foundation/DictionaryPlus.swift
@@ -0,0 +1,18 @@
+//
+//  DictionaryPlus.swift
+//  zeniko
+//
+//  Created by Choi on 2022/10/21.
+//
+
+import Foundation
+
+// MARK: - Optional Value Dictionary
+extension Dictionary where Value: OptionalType {
+    var unwrapped: Dictionary<Key, Value.Wrapped> {
+        reduce(into: [Key:Value.Wrapped]()) { partialResult, tuple in
+            guard let value = tuple.value.optionalValue else { return }
+            partialResult[tuple.key] = value
+        }
+    }
+}
