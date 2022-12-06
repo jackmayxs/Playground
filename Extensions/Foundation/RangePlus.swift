ae2fb84c2d86f8a36d0b0ecf502d64eeab64d5af
Choi <365473321@qq.com>
2022-12-06 16:41:48 +0800

    新增：Range扩展方法

---
+++ Extensions/Foundation/RangePlus.swift
@@ -0,0 +1,28 @@
+//
+//  RangePlus.swift
+//  zeniko
+//
+//  Created by Choi on 2022/12/6.
+//
+
+import Foundation
+
+extension Range {
+    
+    /// 判断左面的范围是否包含右面
+    /// - Returns: 包含则返回true, 否则返回false
+    static func ~=(lhs: Self, rhs: Self) -> Bool {
+        /// clamped -> Always return a smaller range
+        rhs.clamped(to: lhs) == rhs
+    }
+}
+
+extension ClosedRange {
+    
+    /// 判断左面的范围是否包含右面
+    /// - Returns: 包含则返回true, 否则返回false
+    static func ~=(lhs: Self, rhs: Self) -> Bool {
+        /// clamped -> Always return a smaller range
+        rhs.clamped(to: lhs) == rhs
+    }
+}
