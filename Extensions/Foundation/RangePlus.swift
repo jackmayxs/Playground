d8acee5a94dd404b983d4f44f83dfe77a57cdc8a
Choi <365473321@qq.com>
2023-02-27 11:55:57 +0800

    新增：ClosedRange扩展方法

--- Extensions/Foundation/RangePlus.swift
+++ Extensions/Foundation/RangePlus.swift
@@ -8,8 +8,19 @@
 import Foundation
 
 extension ClosedRange where Bound == Int {
+    
     var isSingleRange: Bool {
         upperBound - lowerBound == 0
+    }
+    
+    func fitValueFor(value: Bound) -> Bound {
+        if value < lowerBound {
+            return lowerBound
+        } else if value > upperBound {
+            return upperBound
+        } else {
+            return value
+        }
     }
 }
 
