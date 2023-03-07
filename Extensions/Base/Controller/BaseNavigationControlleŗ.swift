1c4ee8b0649bd2aa7cd7a57ecf0056b39e9119ae
Choi <365473321@qq.com>
2023-03-07 18:04:27 +0800

    优化：导航控制器

--- Extensions/Base/Controller/BaseNavigationControlleŗ.swift
+++ Extensions/Base/Controller/BaseNavigationControlleŗ.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class BaseNavigationController: UINavigationController {
     
+    override init(navigationBarClass: AnyClass?, toolbarClass: AnyClass?) {
+        super.init(navigationBarClass: navigationBarClass, toolbarClass: toolbarClass)
+        initialSetup()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        initialSetup()
+    }
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        initialSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initialSetup() {
+        
+    }
+    
     /// 让最顶部的控制器决定状态栏的样式
     override var childForStatusBarStyle: UIViewController? {
         topViewController?.childForStatusBarStyle ?? topViewController
