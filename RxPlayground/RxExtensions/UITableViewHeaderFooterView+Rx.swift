8e96f7eeff802576425b832179be6e0c7428e283
Choi <365473321@qq.com>
2023-05-04 16:53:46 +0800

    新增：UITableViewHeaderFooterView扩展属性

---
+++ RxPlayground/RxExtensions/UITableViewHeaderFooterView+Rx.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewHeaderFooterView+Rx.swift
+//  KnowLED
+//
+//  Created by Choi on 2023/5/4.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UITableViewHeaderFooterView {
+    var prepareForReuse: Observable<[Any]> {
+        methodInvoked(#selector(UITableViewHeaderFooterView.prepareForReuse))
+    }
+}
