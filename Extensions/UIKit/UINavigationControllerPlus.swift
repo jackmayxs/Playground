20bdcb56324bd74d9b0c4ad52ef0b28dce4f28a0
Choi <365473321@qq.com>
2023-07-11 13:34:52 +0800

    优化UINavigationController扩展方法

--- Extensions/UIKit/UINavigationControllerPlus.swift
+++ Extensions/UIKit/UINavigationControllerPlus.swift
@@ -9,33 +9,26 @@ import UIKit
 extension UINavigationController {
     
     convenience init(@ArrayBuilder<UIViewController> _ controllersBuilder: () -> [UIViewController]) {
-        var controllers = controllersBuilder()
-        guard controllers.isNotEmpty else {
-            self.init()
-            return
-        }
-        let rootController = controllers.removeFirst()
-        self.init(rootViewController: rootController)
-        controllers.forEach { controller in
-            pushViewController(controller, animated: false)
-        }
+        self.init()
+        setViewControllers(controllersBuilder(), animated: false)
     }
     
     /// 新控制器压栈 | 移除所有以前的控制器
     /// - Parameters:
     ///   - newRootController: 新的根控制器
     func refillNavigationStack(_ newRootController: UIViewController, animated: Bool = true) {
-        let previousControllersCount = viewControllers.count
-        pushViewController(newRootController, animated: animated)
-        viewControllers.removeFirst(previousControllersCount)
+        setViewControllers([newRootController], animated: animated)
     }
     
     /// 替换顶层控制器
     /// - Parameters:
     ///   - newController: 顶层控制器
     func replaceTopViewController(_ newController: UIViewController, animated: Bool = true) {
-        let lastTopControllerIndex = viewControllers.count - 1
-        pushViewController(newController, animated: animated)
-        viewControllers.remove(at: lastTopControllerIndex)
+        var tempControllers = viewControllers
+        if tempControllers.first.isValid {
+            tempControllers.removeFirst()
+        }
+        tempControllers.insert(newController, at: 0)
+        setViewControllers(tempControllers, animated: animated)
     }
 }
