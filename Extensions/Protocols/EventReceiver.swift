2554e6d9262b7f00599f6466a6824fbab3fd48ca
Choi <365473321@qq.com>
2023-08-16 09:35:57 +0800

    优化：EventReceiver协议
    - 添加移除Closure回调的方法

--- Extensions/Protocols/EventReceiver.swift
+++ Extensions/Protocols/EventReceiver.swift
@@ -9,7 +9,7 @@ import UIKit
 
 protocol EventReceiver {
     associatedtype Control: UIControl
-    func addEvents(_ events: UIControl.Event, _ callback: ((Control) -> Void)?)
+    func addEvents(_ events: UIControl.Event, _ callback: ((Control) -> Void)?) -> String
 }
 
 extension UIControl: EventReceiver {
@@ -32,17 +32,44 @@ extension UIControl: EventReceiver {
 
 extension EventReceiver where Self: UIControl {
     
-    func addEvents(_ events: UIControl.Event = .touchUpInside, _ callback: ((Self) -> Void)?) {
+    
+    @discardableResult
+    /// 添加Closure回调
+    /// - Parameters:
+    ///   - events: 触发的事件集合
+    ///   - callback: 回调Closure
+    /// - Returns: 标记Action/ClosureSleeve的Identifier, 用于后续移除操作
+    func addEvents(_ events: UIControl.Event = .touchUpInside, _ callback: ((Self) -> Void)?) -> String {
         if #available(iOS 14, *) {
             let action = UIAction { action in
                 guard let callback, let sender = action.sender as? Self else { return }
                 callback(sender)
             }
             addAction(action, for: events)
+            return action.identifier.rawValue
         } else {
             let target = ClosureSleeve(sender: self, callback)
             addTarget(target, action: #selector(target.action), for: events)
             targets.add(target)
+            return target.identifier
+        }
+    }
+    
+    /// 移除回调Closure
+    /// - Parameters:
+    ///   - identifier: 添加时返回的Identifier
+    ///   - events: 相关的事件
+    func removeEvents(identifiedBy identifier: String, for events: UIControl.Event) {
+        if #available(iOS 14.0, *) {
+            removeAction(identifiedBy: UIAction.Identifier(identifier), for: events)
+        } else {
+            let foundSleeve = targets.as(ClosureSleeve<Self>.self).first { sleeve in
+                sleeve.identifier == identifier
+            }
+            if let foundSleeve {
+                removeTarget(foundSleeve, action: #selector(foundSleeve.action), for: events)
+                targets.remove(foundSleeve)
+            }
         }
     }
 }
@@ -53,6 +80,8 @@ final class ClosureSleeve<T> where T: AnyObject {
     weak var sender: T!
     
     var actionCallback: ((T) -> Void)?
+    
+    let identifier = String.random
     
     init(sender: T, _ closure: ((T) -> Void)?) {
         self.sender = sender
