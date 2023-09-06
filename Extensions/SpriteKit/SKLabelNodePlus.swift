4470f7905842e5dfbcb34bdf1139ac8454154d12
Choi <365473321@qq.com>
2023-09-06 10:38:37 +0800

    新增：SKLabelNode扩展方法

--- Extensions/SpriteKit/SKLabelNodePlus.swift
+++ Extensions/SpriteKit/SKLabelNodePlus.swift
@@ -8,12 +8,27 @@ import UIKit
 import SpriteKit
 
 extension SKLabelNode {
-    convenience init(text: String, textColor: UIColor = .white, fontSize: CGFloat = 12.0, fontWeight: UIFont.Weight = .regular) {
+    
+    convenience init(text: String, textColor: UIColor = .white, fontSize: CGFloat, fontWeight: UIFont.Weight = .regular) {
         let attributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.systemFont(ofSize: fontSize, weight: fontWeight),
             .foregroundColor: textColor
         ]
         let attrbutedString = NSAttributedString(string: text, attributes: attributes)
         self.init(attributedText: attrbutedString)
+    }
+    
+    /// 更新文字
+    /// - Parameters:
+    ///   - text: 新文本
+    ///   - textColor: 文本颜色
+    ///   - fontSize: 字体大小
+    ///   - fontWeight: 字重
+    func updateAttributedText(_ text: String, textColor: UIColor, fontSize: CGFloat, fontWeight: UIFont.Weight = .regular) {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: fontSize, weight: fontWeight),
+            .foregroundColor: textColor
+        ]
+        attributedText = NSAttributedString(string: text, attributes: attributes)
     }
 }
