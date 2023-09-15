5e1fe22141ce95e00c90d49aad04e47916d73006
Choi <365473321@qq.com>
2023-09-15 13:46:17 +0800

    新增：Reactive<SKNode>扩展方法

---
+++ RxPlayground/RxExtensions/SKNode+Rx.swift
@@ -0,0 +1,18 @@
+//
+//  SKNode+Rx.swift
+//  KnowLED
+//
+//  Created by Choi on 2023/9/15.
+//
+
+import UIKit
+import SpriteKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: SKNode {
+    
+    var position: Observable<CGPoint> {
+        observe(\.position, options: [.initial, .new])
+    }
+}
