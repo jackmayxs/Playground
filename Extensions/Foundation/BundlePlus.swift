8ca9023fc266debfea1699b1589c8aedf149b93b
Choi <365473321@qq.com>
2023-10-18 14:35:00 +0800

    新增：Bundle扩展属性

--- Extensions/Foundation/BundlePlus.swift
+++ Extensions/Foundation/BundlePlus.swift
@@ -8,17 +8,24 @@ import Foundation
 
 extension Bundle {
     
+    /// 显示名称
     var displayName: String? {
-        infoDictionary?["CFBundleDisplayName"] as? String
+        infoDictionary.flatMap { $0["CFBundleDisplayName"] as? String }
+    }
+    
+    /// 版本号(编译号)
+    var versionWithBuild: String? {
+        guard let version, let build else { return nil }
+        return "\(version)(\(build))"
     }
     
     /// 返回版本号
     var version: String? {
-        infoDictionary?["CFBundleShortVersionString"] as? String
+        infoDictionary.flatMap { $0["CFBundleShortVersionString"] as? String }
     }
     
     /// 返回build号
     var build: String? {
-        infoDictionary?["CFBundleVersion"] as? String
+        infoDictionary.flatMap { $0["CFBundleVersion"] as? String }
     }
 }
