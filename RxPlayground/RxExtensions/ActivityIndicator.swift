0d504f228c46d5db0ce0d271c3e381ea86d62eef
Choi <365473321@qq.com>
2023-11-05 09:25:39 +0800

    新增：ActivityIndicator扩展方法

--- RxPlayground/RxExtensions/ActivityIndicator.swift
+++ RxPlayground/RxExtensions/ActivityIndicator.swift
@@ -42,11 +42,12 @@ public class ActivityIndicator : SharedSequenceConvertibleType {
     private let _loading: SharedSequence<SharingStrategy, Bool>
 
     public init() {
-        _loading = _relay.asDriver()
+        _loading = _relay
+            .asDriver()
             .map { $0 > 0 }
             .distinctUntilChanged()
     }
-
+    
     fileprivate func trackActivityOfObservable<Source: ObservableConvertibleType>(_ source: Source) -> Observable<Source.Element> {
         Observable.using { () -> ActivityToken<Source.Element> in
             self.increment()
@@ -56,9 +57,9 @@ public class ActivityIndicator : SharedSequenceConvertibleType {
         }
     }
 
-    private func increment() {
+    private func increment(_ value: Int = 1) {
         _lock.lock()
-        _relay.accept(_relay.value + 1)
+        _relay.accept(_relay.value + value)
         _lock.unlock()
     }
 
@@ -73,12 +74,38 @@ public class ActivityIndicator : SharedSequenceConvertibleType {
     }
 }
 
+extension ActivityIndicator {
+    
+    /// 只追踪前几个元素
+    /// - Parameters:
+    ///   - elementCount: 追踪的元素个数
+    ///   - source: 信号源
+    /// - Returns: Observable
+    fileprivate func trackActivity<Source: ObservableConvertibleType>(first elementCount: Int, of source: Source) -> Observable<Source.Element> {
+        let afterNext: (Source.Element) -> Void = { _ in
+            self.decrement()
+        }
+        return Observable.using { () -> ActivityToken<Source.Element> in
+            self.increment(elementCount)
+            return ActivityToken(source: source.observable, disposeAction: self.decrement)
+        } observableFactory: { token in
+            token.observable.do(afterNext: afterNext)
+        }
+    }
+}
+
 extension ObservableConvertibleType {
+    
+    public func trackActivity(first elementCount: Int, _ activityIndicator: ActivityIndicator) -> Observable<Element> {
+        activityIndicator.trackActivity(first: elementCount, of: self)
+    }
+    
     public func trackActivity(_ activityIndicator: ActivityIndicator) -> Observable<Element> {
         activityIndicator.trackActivityOfObservable(self)
     }
 }
 extension Completable {
+    
     public func trackActivity(_ activityIndicator: ActivityIndicator) -> Completable {
         activityIndicator.trackActivityOfObservable(self).asCompletable()
     }
