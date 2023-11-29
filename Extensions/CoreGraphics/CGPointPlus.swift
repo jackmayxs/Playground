7b301105d3a72bb3d0bde23db47fc593a51f4ba7
Choi <365473321@qq.com>
2023-11-29 17:52:01 +0800

    新增：CGpoint扩展方法
    + 两点间插入多个点

--- Extensions/CoreGraphics/CGPointPlus.swift
+++ Extensions/CoreGraphics/CGPointPlus.swift
@@ -28,6 +28,33 @@ extension CGPoint {
     func distance(from another: CGPoint) -> CGFloat {
         sqrt(pow(x - another.x, 2.0) + pow(y - another.y, 2.0))
     }
+    
+    /// 在两点之间插入点
+    /// - Parameters:
+    ///   - pointA: 第一个点
+    ///   - pointB: 第二个点
+    ///   - insertCount: 插入点的数量
+    /// - Returns: 当前两点之间新插入的所有点
+    static func generatePoints(between pointA: CGPoint, and pointB: CGPoint, insertCount: Int) -> [CGPoint] {
+        /// 确保插入数量大于0
+        guard insertCount > 0 else { return .empty }
+        /// 创建插入点数组
+        var points: [CGPoint] = []
+        /// 求得xy的偏移
+        let dx = pointB.x - pointA.x
+        let dy = pointB.y - pointA.y
+        /// 求出xy方向上的步进值
+        let stepX = dx / insertCount.cgFloat
+        let stepY = dy / insertCount.cgFloat
+        /// 插入新点
+        for i in 1...insertCount {
+            let x = pointA.x + i.cgFloat * stepX
+            let y = pointA.y + i.cgFloat * stepY
+            let point = CGPoint(x: x, y: y)
+            points.append(point)
+        }
+        return points
+    }
 }
 
 extension CGPoint {
