27abae7623fe66b00bc5428e66695bb6b5f90d00
Choi <365473321@qq.com>
2024-01-08 17:07:34 +0800

    优化协议扩展

--- Extensions/Foundation/Number/DoublePlus.swift
+++ Extensions/Foundation/Number/DoublePlus.swift
@@ -24,22 +24,6 @@ extension Double {
         Int(self * 1000)
     }
     
-    var percentage: String {
-        percentString(fractions: 0) ?? ""
-    }
-    
-    func percentString(fractions: Int = 0) -> String? {
-        NumberFormatter.shared.configure { formatter in
-            formatter.numberStyle = .percent
-            /// 因为在formatter.shared方法里把这个值重置了, 所以这里要在此设置, 否则百分化会失败
-            formatter.positiveSuffix = "%"
-            formatter.minimumFractionDigits = fractions
-            formatter.maximumFractionDigits = fractions
-        }.transform { formatter in
-            formatter.string(from: self.nsNumber)
-        }
-    }
-    
     /// 切分成整数和小数两个部分
     var split: (wholeNumber: Double, fractions: Double) {
         /// 创建Double类型的指针
@@ -50,14 +34,6 @@ extension Double {
         /// 执行整数小数分离 | 把整数部分存入指针
         let fractions = Darwin.modf(self, pWholeNumber)
         return (pWholeNumber.pointee, fractions)
-    }
-    
-    var nsNumber: NSNumber {
-        NSNumber(value: self)
-    }
-    
-    var decimal: Decimal {
-        Decimal(self)
     }
 }
 
@@ -75,104 +51,5 @@ extension Double {
             return 0.0
         }
         return Calendar.gregorian.dateInterval(of: component, for: now)?.duration ?? 0.0
-    }
-}
-
-// MARK: - __________ Format __________
-extension Double {
-    
-    // 默认设置
-    fileprivate var decimalFormatter: NumberFormatter {
-        NumberFormatter.shared.configure { make in
-            /// 数字格式: 小数
-            make.numberStyle = .decimal
-            /// 最大小数位: 2
-            make.maximumFractionDigits = 2
-        }
-    }
-    
-    /// 四舍五入的Formatter
-    fileprivate var roundDecimalFormatter: NumberFormatter {
-        decimalFormatter.configure { make in
-            make.roundingMode = .halfUp
-        }
-    }
-    
-    // 带正负号的Formatter
-    fileprivate var signedDecimalFormatter: NumberFormatter {
-        decimalFormatter.configure { make in
-            make.positivePrefix = "+"
-            make.negativePrefix = "-"
-            make.zeroSymbol = "0" /// 格式化成带有正负号的数字时,0不带符号
-        }
-    }
-    
-    /// 带符号 | 四舍五入
-    var signedR2: String {
-        signedDecimalFormatter.configure { make in
-            make.roundingMode = .halfUp
-        }.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    /// 带符号 | 原样输出
-    var signedF: String {
-        signedDecimalFormatter.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    /// 带符号 | 保留两位小数
-    var signedF2: String {
-        signedDecimalFormatter.configure { make in
-            make.minimumFractionDigits = 2
-        }.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    /// 四舍五入 | 原样输出
-    var r2: String {
-        roundDecimalFormatter.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    /// 原样输出
-    var f: String {
-        decimalFormatter.configure { make in
-            make.minimumFractionDigits = 0
-            make.maximumFractionDigits = .max
-        }.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    /// 保留两位小数的字符串
-    var f2: String {
-        f(2)
-    }
-    
-    var f4: String {
-        f(4)
-    }
-    
-    func signedF(_ fractionDigits: Int) -> String {
-        signedDecimalFormatter.configure { make in
-            make.minimumFractionDigits = fractionDigits
-            make.maximumFractionDigits = fractionDigits
-        }.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
-    }
-    
-    func f(_ fractionDigits: Int) -> String {
-        decimalFormatter.configure { make in
-            make.minimumFractionDigits = fractionDigits
-            make.maximumFractionDigits = fractionDigits
-        }.transform { fmt -> String in
-            fmt.string(from: nsNumber) ?? ""
-        }
     }
 }
--- Extensions/ProtocolExtensions/BinaryFloatingPointPlus.swift
+++ Extensions/ProtocolExtensions/BinaryFloatingPointPlus.swift
@@ -56,6 +56,14 @@ extension BinaryFloatingPoint {
         Double(self)
     }
     
+    var nsNumber: NSNumber {
+        NSNumber(value: double)
+    }
+    
+    var decimal: Decimal {
+        Decimal(double)
+    }
+    
     /// 将自己约束在指定范围内
     /// - Parameter range: 指定范围
     /// - Returns: 约束后的值
@@ -71,5 +79,119 @@ extension BinaryFloatingPoint {
         let divisor = range.upperBound - range.lowerBound
         guard divisor != 0 else { return 0 }
         return (clampedBound - range.lowerBound) / divisor
+    }
+}
+
+extension BinaryFloatingPoint {
+    
+    // 默认设置
+    fileprivate var decimalFormatter: NumberFormatter {
+        NumberFormatter.shared.configure { make in
+            /// 数字格式: 小数
+            make.numberStyle = .decimal
+            /// 最大小数位: 2
+            make.maximumFractionDigits = 2
+        }
+    }
+    
+    /// 四舍五入的Formatter
+    fileprivate var roundDecimalFormatter: NumberFormatter {
+        decimalFormatter.configure { make in
+            make.roundingMode = .halfUp
+        }
+    }
+    
+    // 带正负号的Formatter
+    fileprivate var signedDecimalFormatter: NumberFormatter {
+        decimalFormatter.configure { make in
+            make.positivePrefix = "+"
+            make.negativePrefix = "-"
+            make.zeroSymbol = "0" /// 格式化成带有正负号的数字时,0不带符号
+        }
+    }
+    
+    var percentage: String {
+        percentString(fractions: 0) ?? ""
+    }
+    
+    /// 带符号 | 四舍五入
+    var signedR2: String {
+        signedDecimalFormatter.configure { make in
+            make.roundingMode = .halfUp
+        }.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    /// 带符号 | 原样输出
+    var signedF: String {
+        signedDecimalFormatter.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    /// 带符号 | 保留两位小数
+    var signedF2: String {
+        signedDecimalFormatter.configure { make in
+            make.minimumFractionDigits = 2
+        }.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    /// 四舍五入 | 原样输出
+    var r2: String {
+        roundDecimalFormatter.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    /// 原样输出
+    var f: String {
+        decimalFormatter.configure { make in
+            make.minimumFractionDigits = 0
+            make.maximumFractionDigits = .max
+        }.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    /// 保留两位小数的字符串
+    var f2: String {
+        f(2)
+    }
+    
+    var f4: String {
+        f(4)
+    }
+    
+    func percentString(fractions: Int = 0) -> String? {
+        NumberFormatter.shared.configure { formatter in
+            formatter.numberStyle = .percent
+            /// 因为在formatter.shared方法里把这个值重置了, 所以这里要在此设置, 否则百分化会失败
+            formatter.positiveSuffix = "%"
+            formatter.minimumFractionDigits = fractions
+            formatter.maximumFractionDigits = fractions
+        }.transform { formatter in
+            formatter.string(from: nsNumber)
+        }
+    }
+    
+    func signedF(_ fractionDigits: Int) -> String {
+        signedDecimalFormatter.configure { make in
+            make.minimumFractionDigits = fractionDigits
+            make.maximumFractionDigits = fractionDigits
+        }.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
+    }
+    
+    func f(_ fractionDigits: Int) -> String {
+        decimalFormatter.configure { make in
+            make.minimumFractionDigits = fractionDigits
+            make.maximumFractionDigits = fractionDigits
+        }.transform { fmt -> String in
+            fmt.string(from: nsNumber) ?? ""
+        }
     }
 }
