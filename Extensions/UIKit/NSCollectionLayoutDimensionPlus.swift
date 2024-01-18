fc360bb55f8cf7a8cd43ed9b9a8cb56926d35595
Choi <365473321@qq.com>
2024-01-18 11:38:45 +0800

    新增：NSCollectionLayoutDimension扩展

---
+++ Extensions/UIKit/NSCollectionLayoutDimensionPlus.swift
@@ -0,0 +1,13 @@
+//
+//  NSCollectionLayoutDimensionPlus.swift
+//  KnowLED
+//
+//  Created by Choi on 2024/1/18.
+//
+
+import UIKit
+
+extension NSCollectionLayoutDimension {
+    static let fullWidth = NSCollectionLayoutDimension.fractionalWidth(1.0)
+    static let fullHeight = NSCollectionLayoutDimension.fractionalHeight(1.0)
+}
