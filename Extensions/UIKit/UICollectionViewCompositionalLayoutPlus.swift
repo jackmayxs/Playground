ca57d60eb18bf71ce3156c1ea806c1ad2fd64697
Choi <365473321@qq.com>
2024-01-22 09:54:00 +0800

    UICollectionViewCompositionalLayout扩展文件更名

--- Extensions/UIKit/UICollectionViewCompositionalLayoutPlus.swift
+++ Extensions/UIKit/UICollectionViewCompositionalLayoutPlus.swift
@@ -1,5 +1,5 @@
 //
-//  NSCollectionLayoutDimensionPlus.swift
+//  UICollectionViewCompositionalLayoutPlus.swift
 //  KnowLED
 //
 //  Created by Choi on 2024/1/18.
@@ -10,4 +10,9 @@ import UIKit
 extension NSCollectionLayoutDimension {
     static let fullWidth = NSCollectionLayoutDimension.fractionalWidth(1.0)
     static let fullHeight = NSCollectionLayoutDimension.fractionalHeight(1.0)
+}
+
+
+extension NSCollectionLayoutSize {
+    static let fullSize = NSCollectionLayoutSize(widthDimension: .fullWidth, heightDimension: .fullHeight)
 }
