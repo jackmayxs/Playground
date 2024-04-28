44d1a3730e62ed53485416871b9c679a0a9f4279
Choi <365473321@qq.com>
2024-04-28 11:52:15 +0800

    新增AVPlayer扩展方法

--- Extensions/AVFoundation/AVPlayerPlus.swift
+++ Extensions/AVFoundation/AVPlayerPlus.swift
@@ -10,12 +10,19 @@ import AVFoundation
 extension AVPlayer {
     
     /// 播放器更新进度
-    static func *(lhs: AVPlayer, rhs: Double) {
-        guard let playerItem = lhs.currentItem else { return }
-        let targetPosition = playerItem.duration.seconds * rhs
+    static func *(player: AVPlayer, progress: Double) {
+        guard let playerItem = player.currentItem else { return }
+        let targetPosition = playerItem.duration.seconds * progress
         let time = CMTime(seconds: targetPosition, preferredTimescale: playerItem.duration.timescale)
         guard time.isValid else { return }
-        lhs.seek(to: time)
+        player.seek(to: time)
+    }
+    
+    /// 如果正在播放则先暂停
+    func pauseIfNeeded() {
+        if !rate.isZero {
+            pause()
+        }
     }
     
     /// 如果之前为暂停则开始播放
