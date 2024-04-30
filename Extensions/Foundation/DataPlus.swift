39a265391f31473aa38e719e7857d099e284438e
Choi <365473321@qq.com>
2024-04-30 17:45:11 +0800

    为Data类型的changedBytes方法添加注释

--- Extensions/Foundation/DataPlus.swift
+++ Extensions/Foundation/DataPlus.swift
@@ -24,18 +24,31 @@ public struct ChangedBytes: CustomStringConvertible {
         lowerBound...upperBound
     }
     
-    static func <-- (lhs: [ChangedBytes], rhs: ChangedBytes) -> [ChangedBytes] {
-        guard lhs.isNotEmpty else {
-            return [rhs]
+    /// 合并字节变动数组 | 如果是连续的字节变动则合并
+    /// - Parameters:
+    ///   - lhs: 字节变动数组
+    ///   - rhs: 新的字节变动对象
+    /// - Returns: 合并后的字节变动数组
+    static func <--(changedBytesArray: [ChangedBytes], nextChangedBytes: ChangedBytes) -> [ChangedBytes] {
+        /// 临时字节变化数组拷贝
+        var tempResults = changedBytesArray
+        /// 取出上一次字节变动模型 | 如果为空,说明数组为空,将右侧字节变化包装进数组并返回
+        guard var lastChangedBytes = tempResults.popLast() else {
+            return [nextChangedBytes]
         }
-        var tempResults = lhs
-        guard var tempLast = lhs.last else { return lhs }
-        if rhs.lowerBound == tempLast.upperBound + 1 {
-            tempLast.upperBound = rhs.upperBound
-            _ = tempResults.popLast()
-            tempResults.append(tempLast)
-        } else {
-            tempResults.append(rhs)
+        /// 如果新的字节变动的下限等于上一次字节变动的上线+1, 说明是连续的字节变动
+        if nextChangedBytes.lowerBound == lastChangedBytes.upperBound + 1 {
+            /// 更新上限
+            lastChangedBytes.upperBound = nextChangedBytes.upperBound
+            /// 重新添加回数组
+            tempResults.append(lastChangedBytes)
+        }
+        /// 否则不是连续的字节变动
+        else {
+            /// 重新添加回数组
+            tempResults.append(lastChangedBytes)
+            /// 并拼接新字节变动对象
+            tempResults.append(nextChangedBytes)
         }
         return tempResults
     }
@@ -158,29 +171,65 @@ extension Data {
         binaryInteger(UInt8.self).orZero
     }
     
+    /// 专门用于新旧两个512通道数据帧之间的对比 | 二进制数量不对或新旧二进制count不对都会返回空数组
+    /// - Parameter newData: 新DMX数据帧
+    /// - Returns: 要更新的CRMX数组
+    func changedCRMXES(_ newData: Data) -> CRMXArray {
+        /// 如果二进制相等则直接返回空数组
+        if self == newData { return .empty }
+        /// 确保新旧二进制count相同并都等于512个通道
+        guard count == newData.count, count == DMX.fullRange.count else { return .empty }
+        /// 变动的字节
+        return changedBytes(newData: newData).reduce(into: CRMXArray.empty) { array, next in
+            #warning("需要根据MTU合并二进制")
+            array.append(contentsOf: next.crmxDatas)
+        }
+    }
+    
     /// 比较相同数量的二进制对象, 返回变化的字节数组
-    /// - Parameter newData: 新数组
+    /// 注: 如果是连续的字节变化会进行合并操作
+    /// - Parameter newData: 新二进制
     /// - Returns: 变化的数组,带变化的索引
+    /// 实例: [0,0,0,0,0,0,0,0,0,0] -> [255,255,255,0,0,255,255,0,0,255]
+    /// 结果: [changed: 0 - 2: [255, 255, 255], changed: 5 - 6: [255, 255], changed: 9 - 9: [255]
     func changedBytes(newData: Data) -> [ChangedBytes] {
-        let count = Swift.min(self.count, newData.count)
+        /// 取出较小的数量
+        let count = Swift.min(count, newData.count)
+        /// 临时二进制变化数组
         var changedBytesArray: [ChangedBytes] = []
-        for i in 0 ..< count {
-            let oldByte = self[i]
-            let newByte = newData[i]
+        /// 遍历每个二进制
+        for index in 0..<count {
+            /// 旧字节
+            let oldByte = self[index]
+            /// 新字节
+            let newByte = newData[index]
+            /// 如果字节有变动
             if newByte != oldByte {
+                /// 取出上一段变动的字节模型
                 guard var temp = changedBytesArray.last else {
-                    let newItem = ChangedBytes(lowerBound: i, upperBound: i, bytes: [newByte])
+                    /// 如果还没有上一段变动的字节,则生成一个新的
+                    let newItem = ChangedBytes(lowerBound: index, upperBound: index, bytes: [newByte])
+                    /// 并添加到数组
                     changedBytesArray.append(newItem)
+                    /// 继续循环
                     continue
                 }
+                /// 上一段字节变动的上限+1
                 let nextUpperBound = temp.upperBound + 1
-                if i == nextUpperBound {
+                /// 如果当前的Index等于上一次变动的上线+1,说明是连续的字节变动
+                if index == nextUpperBound {
+                    /// 更新上一次字节变动的上限
                     temp.upperBound = nextUpperBound
+                    /// 并添加新的字节
                     temp.bytes.append(newByte)
+                    /// 移除旧值
                     _ = changedBytesArray.popLast()
+                    /// 添加新值
                     changedBytesArray.append(temp)
-                } else {
-                    let newItem = ChangedBytes(lowerBound: i, upperBound: i, bytes: [newByte])
+                }
+                /// 否则为不连续的字节变动,创建新值并添加到数组
+                else {
+                    let newItem = ChangedBytes(lowerBound: index, upperBound: index, bytes: [newByte])
                     changedBytesArray.append(newItem)
                 }
             }
