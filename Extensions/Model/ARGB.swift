8b758153f54b379a935db2815bcc5eb0dca57290
Choi <365473321@qq.com>
2024-06-20 18:04:01 +0800

    新增：ARGB遵循Comparable协议

--- Extensions/Model/ARGB.swift
+++ Extensions/Model/ARGB.swift
@@ -16,6 +16,8 @@ struct ARGB {
 
 extension ARGB {
     
+    static let black = ARGB(red: 0.0, green: 0.0, blue: 0.0)
+    
     init(red: Double, green: Double, blue: Double) {
         self.alpha = 1.0
         self.red = red
@@ -32,5 +34,26 @@ extension ARGB {
     
     var rgbArray: [Double] {
         [red, green, blue]
+    }
+}
+
+extension ARGB: Comparable {
+    
+    /// 比较两个颜色
+    static func < (lhs: ARGB, rhs: ARGB) -> Bool {
+        let lRed = lhs.red, lGreen = lhs.green, lBlue = lhs.blue
+        let rRed = rhs.red, rGreen = rhs.green, rBlue = rhs.blue
+        if lRed != rRed {
+            return lRed < rRed
+        }
+        else if lGreen != rGreen {
+            return lGreen < rGreen
+        }
+        else if lBlue != rBlue {
+            return lBlue < rBlue
+        }
+        else {
+            return false
+        }
     }
 }
