94e3fde7f5d9f79a720a136a01800b1e7fedc9e8
Choi <365473321@qq.com>
2024-06-22 11:38:07 +0800

    优化AutoResizeCollectionView

--- Extensions/CustomizedControls/AutoResizeCollectionView.swift
+++ Extensions/CustomizedControls/AutoResizeCollectionView.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class AutoResizeCollectionView: UICollectionView {
+    
 	override var contentSize: CGSize {
 		get { super.contentSize }
-		set {
-			super.contentSize = newValue
+		set { super.contentSize = newValue
 			invalidateIntrinsicContentSize()
 		}
 	}
+    
 	override var intrinsicContentSize: CGSize { contentSize }
 }
