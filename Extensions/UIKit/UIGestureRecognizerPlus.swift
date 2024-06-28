c4297934c669f4c26dbc561abf619307c1db0556
Choi <365473321@qq.com>
2024-06-28 10:26:22 +0800

    新增：UIGestureRecognizer扩展方法

--- Extensions/UIKit/UIGestureRecognizerPlus.swift
+++ Extensions/UIKit/UIGestureRecognizerPlus.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+extension UIGestureRecognizer {
+    /// 将任意类型的手势转换成普通的UIGestureRecognizer
+    /// 用于响应式编程中合并两种不同的手势事件
+    /// e.g. Observable.merge(pan.rx.event.map(\.mediocreGestureRecognizer), tap.rx.event.map(\.mediocreGestureRecognizer))
+    var mediocreGestureRecognizer: UIGestureRecognizer {
+        self
+    }
+}
+
 extension UIGestureRecognizer.State {
     
     /// 用于过滤手指停止事件
@@ -15,5 +24,29 @@ extension UIGestureRecognizer.State {
     }
     var isEnded: Bool {
         self == .ended
+    }
+}
+
+extension UIGestureRecognizer.State: CustomStringConvertible {
+    
+    public var description: String {
+        switch self {
+        case .possible:
+            "possible"
+        case .began:
+            "began"
+        case .changed:
+            "changed"
+        case .ended:
+            "ended"
+        case .cancelled:
+            "cancelled"
+        case .failed:
+            "failed"
+        case .recognized:
+            "recognized"
+        @unknown default:
+            ""
+        }
     }
 }
