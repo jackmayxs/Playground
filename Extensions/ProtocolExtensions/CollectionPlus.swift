499a0bd95f82298e91ce209dfcd42e317f24fa49
Choi <365473321@qq.com>
2024-07-05 17:57:29 +0800

    新增：Collection扩展属性

--- Extensions/ProtocolExtensions/CollectionPlus.swift
+++ Extensions/ProtocolExtensions/CollectionPlus.swift
@@ -13,6 +13,16 @@ extension Collection {
         isNotEmpty ? self : defaultCollection
     }
     
+    /// 只包括一个元素或无元素
+    var containsSingleElementOrEmpty: Bool {
+        count <= 1
+    }
+    
+    /// 只包含一个元素
+    var containsSingleElement: Bool {
+        count == 1
+    }
+    
     /// 如果为空则返回nil
     var filledOrNil: Self? {
         isNotEmpty ? self : nil
