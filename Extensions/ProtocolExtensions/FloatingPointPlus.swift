9fcb1c3293447b9c6a652202f57614969d2d5598
Choi <365473321@qq.com>
2024-09-09 14:56:55 +0800

    新增：FloatingPoint协议扩展方法

--- Extensions/ProtocolExtensions/FloatingPointPlus.swift
+++ Extensions/ProtocolExtensions/FloatingPointPlus.swift
@@ -11,4 +11,14 @@ extension FloatingPoint {
     
     /// 实时创建的百分比范围, 尽量避免直接使用. 在常用的类型扩展里储存一份静态常量
     static var hotPercentRange: ClosedRange<Self> { 0...1 }
+    
+    /// 按照指定精度和进位规则进位
+    /// - Parameters:
+    ///   - roundingIncrement: 进位精度
+    ///   - rule: 进位规则
+    /// - Returns: 进位后的结果
+    /// 这里的形参命名借用了NumberFormatter的同名属性, 具体作用在其reset()方法中的属性注释中有详细说明
+    func rounded(_ roundingIncrement: Self, rule: FloatingPointRoundingRule) -> Self {
+        roundingIncrement * (self / roundingIncrement).rounded(rule)
+    }
 }
