4c937dc8aa8e656120080d2b546b60dff0fa673d
Choi <365473321@qq.com>
2024-09-09 16:55:57 +0800

    新增typealias

--- Extensions/Foundation/TypeAlias.swift
+++ Extensions/Foundation/TypeAlias.swift
@@ -13,6 +13,7 @@ public typealias UUIDArray = Array<UUID>
 public typealias UUIDSet = Set<UUID>
 public typealias IntRange = ClosedRange<Int>
 public typealias IntRanges = Array<IntRange>
+public typealias DoubleRange = ClosedRange<Double>
 public typealias IndexPathSet = Set<IndexPath>
 public typealias SimpleCallback = () -> Void
 public typealias CompletedCallback = (Swift.Error?) -> Void
