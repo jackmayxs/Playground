2e0892907ac21b8522e9112b6148f78cf3128615
Choi <365473321@qq.com>
2024-09-26 11:35:25 +0800

    新增：信号强度百分比

--- Extensions/CoreBluetooth/Int+RSSI.swift
+++ Extensions/CoreBluetooth/Int+RSSI.swift
@@ -10,14 +10,19 @@ import Foundation
 
 extension Int {
     
+    /// 信号强度百分比
+    var rssPercent: Double {
+        rssLevel.double / 4.0
+    }
+    
     /// 信号强度等级 | 0...4由弱到强
     var rssLevel: Int {
         switch self {
-        case (-55)...0     : return 4
-        case (-65)..<(-55) : return 3
-        case (-75)..<(-65) : return 2
-        case (-85)..<(-75) : return 1
-        case      ..<(-85) : return 0
+        case -55...0     : return 4
+        case -65..<(-55) : return 3
+        case -75..<(-65) : return 2
+        case -85..<(-75) : return 1
+        case    ..<(-85) : return 0
         default:
             assertionFailure("WRONG RSSI: (\(self)), CHECK YOUR SOURCE.")
             return 0
