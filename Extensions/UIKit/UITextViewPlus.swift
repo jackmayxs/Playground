df64ac7fb514fb4cbf3a69b9f9118de9b7b58546
Choi <365473321@qq.com>
2024-10-20 15:42:06 +0800

    优化：UITextView计算高度的方法

--- Extensions/UIKit/UITextViewPlus.swift
+++ Extensions/UIKit/UITextViewPlus.swift
@@ -10,14 +10,8 @@ import UIKit
 extension UITextView {
     
     // Note: This will trigger a text rendering!
-    var calculateViewHeightWithCurrentWidth: CGFloat {
-        let textWidth = frame.width -
-        textContainerInset.left -
-        textContainerInset.right -
-        textContainer.lineFragmentPadding * 2.0 -
-        contentInset.left -
-        contentInset.right
-        
+    var viewHeightForCurrentWidth: CGFloat {
+        let textWidth = bounds.width - textContainerInset.horizontal - textContainer.lineFragmentPadding * 2.0 - contentInset.horizontal
         let maxSize = CGSize(width: textWidth, height: CGFloat.greatestFiniteMagnitude)
         let tmpAttributedString: NSAttributedString
         if let attributedText, attributedText.length > 0 {
@@ -27,8 +21,7 @@ extension UITextView {
         }
         let options: NSStringDrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
         var calculatedSize = tmpAttributedString.boundingRect(with: maxSize, options: options, context: nil).size
-        calculatedSize.height += textContainerInset.top
-        calculatedSize.height += textContainerInset.bottom
+        calculatedSize.height += textContainerInset.vertical + contentInset.vertical
         return ceil(calculatedSize.height)
     }
 }
